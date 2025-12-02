a87ac79c1834e0dbf9a730c1f1a86d7ee7540504
thangvu <thangducvu777@gmail.com>
2025-12-02 11:59:39 -0600

    update

--- src/main/java/com/example/demo/domain/vo/EmployeeVO.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.domain.vo;
-
-import java.math.BigDecimal;
-
-public record EmployeeVO(String name, BigDecimal salary, String department) {}
--- src/main/java/com/example/demo/domain/vo/TransactionVO.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.domain.vo;
-
-import java.math.BigDecimal;
-import java.math.BigInteger;
-
-public record TransactionVO (BigInteger id, String type, BigDecimal amount, String status) {
-}
